d96c4efb06cdcfba7d76b101d2b97a6e18701eab
hillarytizian-boop <hillarytizian-boop@users.noreply.github.com>
2026-04-09 11:05:20 +0300

    Fix backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
-
 const api = axios.create({
-  baseURL: 'https://rental-management-system-u2wf.onrender.com/api',
+  baseURL: 'https://rental-management-system-u2wf.onrender.com/api'
 });
-
 const token = localStorage.getItem('token');
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
-
+if (token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 export default api;
